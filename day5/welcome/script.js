c41ba3dcd756f72771bb32477071856fba0e373d
BasmaEsmail <52723710+BasmaEsmail@users.noreply.github.com>
2022-05-05 23:39:37 +0200

    Update script.js

--- day5/welcome/script.js
+++ day5/welcome/script.js
@@ -1,8 +1,8 @@
-var data = location.search.substring(1);
+var data = location.search.slice(1);
 var values = data.split("&");
 var header = document.createElement("h1");
-header.innerHTML="Welcome, "+values[0].substring(6);
+header.innerHTML="Welcome, "+values[0].slice(6);
 var email= document.createElement("h2");
-email.innerHTML="Email: " + values[1].substring(6);
+email.innerHTML="Email: " + values[1].slice(6,values[1].indexOf('%'))+"@"+values[1].slice(values[1].indexOf('%')+3);
 document.body.appendChild(header);
 document.body.appendChild(email)
